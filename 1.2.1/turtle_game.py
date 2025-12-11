92092e4c93c4d411c539c4e6085877868f4c533f
Owen T <owen.torppey@vtsd.com>
2025-12-11 13:27:11 -0500

    updated 1.2.2

--- 1.2.1/turtle_game.py
+++ 1.2.1/turtle_game.py
@@ -1,33 +1,41 @@
-
 #-----import statements-----
 import turtle as trtl
 import random as rand
+#import leaderboard as lb
 
 #-----game configuration----
-spot_color = "red"
+spot_color = "navy"
 score = 0
-font_setup = ("Arial", 20, "normal")
+font_setup = ("papyrus", 20, "normal")
+timer = 30
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
 
 #-----initialize turtle-----
-#The score turtle
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name = input("Enter your name?")
+
+# Score turtle
 score_writer = trtl.Turtle()
-#The box turtle
-box_turtle = trtl.Turtle()
 score_writer.penup()
-score_writer.hideturtle()
-box_turtle.hideturtle()
-pim = trtl.Turtle()
-pim.shape("circle")
-pim.color(spot_color)
-pim.shapesize(3)
-pim.penup()
-box_turtle.speed(0)
-score_writer.speed(0)
+counter =  trtl.Turtle()
+counter.penup()
+
+# Turtle to draw a box
+box_turtle = trtl.Turtle()
+box_turtle.penup()
+
+# Shape turtle
+meowl = trtl.Turtle()
+meowl.shape("circle")
+meowl.color(spot_color)
+meowl.shapesize(3.5)
+meowl.penup()
+
 #-----game functions--------
 # Draw the box for the score
 def scoreBox():
     # Set up the starting location and pendown
-    box_turtle.penup()
     box_turtle.goto(275, 325)
     box_turtle.pendown()
 
@@ -39,18 +47,25 @@ def scoreBox():
         box_turtle.left(90)
 
     # Place score_writer where it will write the score
-    box_turtle.penup()
+    score_writer.penup()
     score_writer.goto(300, 332)
+
+    # Hide the turtles
+    score_writer.hideturtle()
+    box_turtle.hideturtle()
 
 # Get a score boost, move the turtle randomly
 def spot_clicked(x, y):
-    change_position()
+    global timer_up
+    if timer_up == False:
+        change_position()
+    else:
+        meowl.hideturtle()
 
 def change_position():
     # Move the turtle to a random location
     newX = rand.randint(-300, 300)
     newY = rand.randint(-300, 300)
-    meowl.speed(0)
     meowl.goto(newX, newY)
     update_score()
 
@@ -59,13 +74,60 @@ def update_score():
     global score
     # Increment the score by 1
     score += 1
+    # Clear out the prior score
     score_writer.clear()
-    # print the score
+    # Print the current score
     score_writer.write(score, font=font_setup)
+
+# Counter setup
+def counter_setup():
+    counter.forward(-200)
+    counter.left(90)
+    counter.forward(300)
+    counter.right(90)
+    counter.hideturtle()
+
+# Start the countown and update it each frame
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+    manage_leaderboard()
+
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval)
+
+# CODE TO ADD
+# Add this function to your game code
+
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+
+  global score
+  global moewl
+
+  # get the names and scores from the leaderboard file
+  leader_names_list = lb.get_names(leaderboard_file_name)
+  leader_scores_list = lb.get_scores(leaderboard_file_name)
+
+  # show the leaderboard with or without the current player
+  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, moewl, score)
+
+  else:
+    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, moewl, score)
+
 
 #-----events----------------
 meowl.onclick(spot_clicked)
-
+counter_setup()
 scoreBox()
 wn = trtl.Screen()
+wn.bgcolor("maroon")
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()

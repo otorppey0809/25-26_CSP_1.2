13eec4cf2eb80f744f240a866794803bb2913e7b
Owen T <owen.torppey@vtsd.com>
2025-12-17 13:08:48 -0500

    updated 1.2.2

---
+++ 1.2.2/peramiter practice/example1.py
@@ -0,0 +1,11 @@
+def add_numbers(one,two):
+    sum = one + two
+    return sum
+
+print(add_numbers(20,21))
+
+def multiply_numbers(one, two, three):
+    product = (one + two) * three
+    return product
+
+print(multiply_numbers(20,21,22))

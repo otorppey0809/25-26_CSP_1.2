9ebde84f8e49e8b84ffcfe9fb70807b5b48796e5
Owen T <owen.torppey@vtsd.com>
2026-01-08 13:10:47 -0500

    updated 1.2.2

--- 1.2.3/main.py
+++ 1.2.3/main.py
@@ -1,62 +1,88 @@
-#   a123_apple_1.py
 import turtle as trtl
+import random as rand
 
-#-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
-
+ground_height = -800
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
+letter = "a"
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 
+wn.bgpic("background.gif")
 apple = trtl.Turtle()
+drawer = trtl.Turtle()
+drawer.hideturtle()
+apple.penup()
 
-#-----functions-----
+
+def reset_apple(active_apple):
+    length_of_list = len(letters)
+    if(length_of_list != 0):
+        index = rand.randint(0,length_of_list)
+#Make list for letters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+index = rand.randint(0,len(letters))
+current_letter = letters.pop(index)
+draw_apple(active_apple, activate_apple)
+
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
-  wn.update()
+  wn.tracer(False)
+  draw_letter(current_letter, active_apple)
+  wn.tracer(True)
 
-drawer = trtl.Turtle()
+def drop_apple():
+  wn.tracer(True)
+  apple.goto(apple.xcor(), ground_height)
+  wn.tracer(False)
+  reset_apple(apple)
 
-# This function takes care of font and color.
-def draw_an_A():
-  drawer.color("blue")
-  drawer.write("A", font=("Arial", 74, "bold"))
+  drawer.clear()
+  drawer.hideturtle()
 
-# This call to the onkeypress function sets draw_an_A as the function
-# that will be called when the "a" key is pressed.
-wn.onkeypress(draw_an_A, "a")
-
-import turtle as trtl
-
-drawer1 = trtl.Turtle()
-drawer2 = trtl.Turtle()
-drawer3 = trtl.Turtle()
-
-wn = trtl.Screen()
-
-wn.tracer(False)
-
-drawer1.setheading(45)
-drawer1.forward(200)
-
-wn.tracer(True)
-
-drawer2.setheading(315)
-drawer2.forward(200)
-
-wn.tracer(False)
-
-drawer3.setheading(135)
-drawer3.forward(200)
-
-wn.listen()
-
-
-wn.mainloop()
-
-#-----function calls-----
+def draw_letter(letter, drawer):
+  drawer.color("white")
+  remember_position = drawer.position()
+  drawer.setpos(drawer.xcor() + apple_letter_x_offset,drawer.ycor() + apple_letter_y_offset)
+  drawer.write(letter, font=("Arial", 74, "bold"))
+  drawer.setpos(remember_position)
 
 draw_apple(apple)
 
-wn.mainloop()
+#   a123_apple_and_letters.py
+#TODO Create a function that takes a turtle as its parameter and gives that turtle (apple)
+# a new location on the tree, only if the list of letters is not empty.
+
+#TODO Create a function that draws a new letter from the letter list.
+
+#TODO Create a function that takes a turtle (apple) as its parameter
+# and set that turtle to be shaped by the image file, call the letter drawing function,
+# and update the Screen
+
+#TODO Iterate over the numbers from 0 to the number of apples, creating that many turtles
+# calling your function that resets the apples by giving them a new random location
+# add the new apples to a list of apples to be used in the rest of the program.
+# The loop below executes the correct number of times by using the range() function
+# to create a list of numbers to iterate over.
+for i in range(0, letters):
+    #Your code here
+
+#TODO Create a function that takes a letter as its parameter, retrieve a
+# random turtle (apple) and causes the turtle (apple) to drop from the tree and the letter
+# to disappear. Call the apple reseting function.
+
+#TODO define a function per letter that you will use in your program. Each function should check
+# to see if the given letter is in the list of letters; if it is, it should drop an apple at random.
+
+#TODO use the onkeypress method of wn to correlate the functions you defined above with each
+# of the letters that the user might type.
+# onkeypress requires that you name one function that must take
+# no arguments to be called when the specified key is pressed.
+
+wn.onkeypress(drop_apple, "a")
+
+wn.listen()
+trtl.mainloop()
